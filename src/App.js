9b2fcf379c2087ce65870ca6c348107493c63dcb
Shubham191297 <thapliyalshubham8@gmail.com>
2024-03-18 13:39:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Container from "./components/Container";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Container.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useEffect } from "react";
+import GiphyCard from "./GiphyCard";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData } from "../store/listgiphy-actions";
+
+const Container = () => {
+  const listGiphy = useSelector((state) => state.gifList);
+  const dispatch = useDispatch();
+
+  const sendRequest = async () => {
+    dispatch(fetchData());
+  };
+
+  useEffect(() => {
+    sendRequest();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="container text-center" style={{ color: "white" }}>
+      <div className="row my-4">
+        {listGiphy.map((item, index) => {
+          return (
+            <div className="col col-md-3 my-2" key={index}>
+              <GiphyCard
+                key={item.id}
+                title={item.title}
+                imageURL={item.embed_url}
+                username={item.username}
+                import_datetime={item.import_datetime}
+                trending_datetime={item.trending_datetime}
+                browseURL={item.url}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Container;
